313ec5541ef387c87fbc9c8eb5775111a8f8f1da
hwikookchoe <naracar12@gmail.com>
2022-06-16 19:43:40 -0500

    First commit of BOJ

---
+++ Baekjoon/1697.py
@@ -0,0 +1,13 @@
+def f(n, k):
+    if n >= k:
+        return n - k
+    elif k == 1:
+        return 1
+    elif k % 2:
+        return min(f(n, k-1), f(n, k + 1)) + 1
+    else:
+        return min(k - n, f(n, k // 2) + 1)
+
+
+N, K = map(int, input().split())
+print(f(N, K))
