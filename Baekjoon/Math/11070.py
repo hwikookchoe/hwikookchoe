1e72f99e32a38015d1761a7afa429ab3899207ba
hwikookchoe <naracar12@gmail.com>
2022-06-18 11:18:49 -0500

    BOJ

---
+++ Baekjoon/Math/11070.py
@@ -0,0 +1,15 @@
+# https://www.acmicpc.net/problem/11070
+import sys
+I = sys.stdin.readline
+
+for _ in [0]*int(I()):
+    N, M = map(int, I().split())
+    R = [[0, 0] for _ in [0]*(N+1)]
+    for _ in [0]*M:
+        a, b, p, q = map(int, I().split())
+        R[a][0] += p
+        R[a][1] += q
+        R[b][0] += q
+        R[b][1] += p
+    R = [0 if s**2+a**2 == 0 else int((s**2)*1000/(s**2+a**2)) for s, a in R[1:]]
+    sys.stdout.write(f'{max(R)}\n{min(R)}\n')
